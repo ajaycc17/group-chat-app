68bea04cad640445a20fffe64264d8d9e060f4cb
Ajay Choudhury <ajaycc17@gmail.com>
2023-08-03 00:17:41 +0530

    use localhost for reducing load on db

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -2,9 +2,11 @@ const Message = require("../models/message");
 const user = require("../models/user");
 
 exports.getAllMessages = async (req, res) => {
+    const off = Number(req.query.last) || 0;
     try {
         const messages = await Message.findAll({
-            order: [["createdAt", "ASC"]],
+            offset: off,
+            order: [["createdAt", "DESC"]],
         });
         res.status(200).json(messages);
     } catch (err) {
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,49 +3,76 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import AllGrps from "./components/AllGrps";
 import Chat from "./components/Chat";
-import ChatMine from "./components/ChatMine";
+
+const dataArr: (string | null)[] = [];
 
 export default function Home() {
     const [message, setMessage] = useState("");
     const [effectLogic, setEffectLogic] = useState(false);
-    const [messData, setMessData] = useState([]);
+    let lastIdNum = 0;
+    if (localStorage.getItem(String(0)) !== null) {
+        const theData = JSON.parse(localStorage.getItem(String(0))!);
+        lastIdNum = theData.id;
+    }
+    const [lastId, setLastId] = useState(lastIdNum);
+    const [messData, setMessData] = useState([] as (string | null)[]);
     let baseUrl = "http://localhost:3000";
 
+    // only on initial render
+    useEffect(() => {
+        if (localStorage.getItem(String(0)) !== null) {
+            for (let i = 0; i < 10; i++) {
+                dataArr.push(JSON.parse(localStorage.getItem(String(i))!));
+            }
+        }
+    }, []);
+
+    // every second check
     useEffect(() => {
         const token = localStorage.getItem("token");
         // get expense and downloadeds data together
-        let url = baseUrl + `/message`;
+        let url = baseUrl + `/message?last=${lastId}`;
         axios
             .get(url, {
                 headers: { Authorization: token },
             })
             .then((res) => res.data)
             .then((data) => {
-                // console.log(data);
-                setMessData(data);
+                const limit = data.length > 10 ? 10 : data.length;
+                for (let i = 0; i < limit; i++) {
+                    localStorage.setItem(String(i), JSON.stringify(data[i]));
+                    dataArr[i] = data[i];
+                }
             })
             .catch((err) => console.log(err));
     }, [effectLogic]);
 
+    // update every second
+    setInterval(() => {
+        setEffectLogic(!effectLogic);
+    }, 1000);
+
     // handle message submission
-    const handleSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
+    const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         let url = baseUrl + "/message/add";
         const token = localStorage.getItem("token");
-        try {
-            await axios.post(
+        axios
+            .post(
                 url,
                 { message: message },
                 {
                     headers: { Authorization: token },
                 }
-            );
-            setMessage("");
-            setEffectLogic(!effectLogic);
-        } catch (err) {
-            console.log(err);
-        }
+            )
+            .then(() => {
+                setMessage("");
+                setEffectLogic(!effectLogic);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
     return (
         <main className="px-2 md:px-4">
@@ -56,9 +83,8 @@ export default function Home() {
                     <AllGrps />
                 </div>
                 <div className="col-span-8 py-4 h-[calc(100vh-87px)]">
-                    <div className="chat-body rounded-t-xl p-4 relative h-[calc(100%-4.4rem)] flex flex-col justify-end gap-2">
-                        {messData.map((messItem: { content: string }) => {
-                            console.log(messItem);
+                    <div className="chat-body rounded-t-xl p-4 relative h-[calc(100%-4.4rem)] flex flex-col-reverse gap-2 overflow-y-scroll">
+                        {dataArr.map((messItem: any) => {
                             return <Chat data={messItem.content} />;
                         })}
                     </div>
